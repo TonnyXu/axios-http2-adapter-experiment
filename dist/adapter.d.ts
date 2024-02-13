7c32eaa3873e96c8394c4a93b22bed4302b2f41f
Tonny Xu <tonny.xu@gmail.com>
2024-02-13 18:05:38 +0900

    removed dist folder

--- dist/adapter.d.ts
+++
@@ -1,7 +0,0 @@
-import { AxiosAdapter } from 'axios';
-import * as http2 from 'http2-wrapper';
-export interface HTTP2AdapterConfig {
-    agent?: http2.Agent;
-    force?: boolean;
-}
-export declare function createHTTP2Adapter(adapterConfig?: Partial<HTTP2AdapterConfig>): AxiosAdapter;
